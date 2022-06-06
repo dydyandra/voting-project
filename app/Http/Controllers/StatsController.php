709344d54e145aeb4629d7b07ca77b5f96faebb3
Dyandra Paramitha <73653209+dydyandra@users.noreply.github.com>
2022-06-06 13:49:38 +0700

    add statistics

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -5,34 +5,39 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Voting;
+use Illuminate\Support\Carbon;
 
 class StatsController extends Controller
 {
     public function index(){
         
-        
+        // get USER for all
         $user = User::all();
         $user_count = $user->count();
 
+        // get USER for only today
+        $userToday = User::whereDate('created_at', Carbon::today())->get();
+        $userToday_count = $userToday->count();
+
+        // get Voting for all
         $hasVoted = Voting::all();
         $voting_count = $hasVoted->count();
-        
 
-        $hasNotVoted = $user_count - $voting_count;
+        // get voting only for today
+        $votingToday = Voting::whereDate('created_at', Carbon::today())->get();
+        $votingToday_count  = $votingToday->count();
 
-        // $kandidat_1 = Voting::where('kandidat_id','1')->get();
-    	// $kandidat_2 = Voting::where('kandidat_id','2')->get();
-    	// $kandidat_3 = Voting::where('kandidat_id','3')->get();
 
-    	// $kandidat_1_count = count($kandidat_1);    	
-    	// $kandidat_2_count = count($kandidat_2);
-    	// $kandidat_3_count = count($kandidat_3);
-        // $kandidat_total = 
-    	
+        // get VOTING who has not
+        $hasNotVoted = $user_count - $voting_count; 
+    	$hasNotVotedPercent = round(($hasNotVoted/$user_count)*100, 2);
         return view('stats', [
             'registered_count' => $user_count,
             'voting_count' => $voting_count,
             'hasNotVoted' => $hasNotVoted,
+            'registered_today_count' => $userToday_count,
+            'voted_today_count' => $votingToday_count,
+            'hasNotVotedPercent' => $hasNotVotedPercent
         ]);
 
     }
