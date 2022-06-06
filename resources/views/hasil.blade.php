565e65f69c700cba7eb97681071ad0ee7d00620f
Dyandra Paramitha <73653209+dydyandra@users.noreply.github.com>
2022-06-06 20:57:24 +0700

    add hasil voting

--- resources/views/hasil.blade.php
+++ resources/views/hasil.blade.php
@@ -1,15 +1,18 @@
 @extends('layouts.template')
 
 @section('container')
-<article class="mb-5">
-    <h3>{{ $result->kandidat->nama }}</h3>
-
-    <h6>
-        <a href="/kandidat/detail/{{ $result->kandidat->id }}" class="text-decoration-none"> {{ $result->kandidat->nama  }} </a>
-    </h6>
-
-    {!! $result->kandidat->keterangan !!}
-
-    <a href="/articles" class="d-block mt-3">Back</a>
-</article>
+<div class="mt-5">
+<div class="d-flex justify-content-center">
+<div class="card w-50">
+    <div class="card-header"><h4>Anda telah melakukan voting.</h4></div> 
+    <div class="card-body">
+        <h3 class="card-title"><a href="/voting/{{ $result->kandidat->slug }}" class="text-decoration-none"> {{ $result->kandidat->nama  }} </a></h3>
+        <p class="card-text">{{ $result->kandidat->keterangan}}</p>
+        <a href="/articles" class="btn btn-primary">Go back</a>
+      </div>
+      <img src="{{ asset('storage/images/' . $result->kandidat->photo) }}" class="card-img-bottom" alt="...">
+  </div>
+</div>
+</div>
 @endsection
+
